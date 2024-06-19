435afc0ebd6cdced88b307ca19899291c2447012
Matthew Luk <matthew.luk@yahoo.com>
2024-06-18 22:41:36 -0700

    deleted uneeded imports

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import { useState } from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css';
-import Main from './Components/Main';
-import Text from './Components/Text';
-import { SiPokemon } from "react-icons/si";
 import Mobile from './Components/Mobile';
 import SingleView from './Components/SingleView';
+import Script from './Components/Script';
 
 function App() {
-  const [searchPokemon, setSearchPokemon] = useState("")
-  const [pokemonId, setPokemonId] = useState("")
-  const [version, setVersion] = useState(1)
+  const [pokemonId, setPokemonId] = useState(0)
 
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          {/* <SiPokemon size={"24rem"}/>
-          <Main searchPokemon={searchPokemon}/>
-          <Text setSearchPokemon={setSearchPokemon}/> */}
           <Route path='/' element={<Mobile pokemonId={pokemonId} setPokemonId={setPokemonId}/>}/>
           <Route path={`/${pokemonId}`} element={<SingleView pokemonId={pokemonId} setPokemonId={setPokemonId}/>} />
+          <Route path='/test' element={<Script />} />
+          <Route path='*' element={<SingleView pokemonId={pokemonId} setPokemonId={setPokemonId}/>} />
         </Routes>
       </BrowserRouter>
     </div>
